2b28a8d6d2edd119641da26c9ca4d4eeca29df83
xiangyun-kongxy <xiangyun.kongxy@gmail.com>
2018-06-09 14:36:50 +0800

    init

--- src/learning/repeat_learning/distil/bin_combine.hpp
+++ src/learning/repeat_learning/distil/bin_combine.hpp
@@ -23,8 +23,8 @@ namespace mind {
                 list<ptr<barren>> brs;
                 for (int i = 0; i < books[0]->size() - 1; ++i) {
                     brs.push_back(new barren({m_id_relate,
-                                        (**books[0])[i]->id(),
-                                        (**books[0])[i+1]->id()}));
+                                  (**books[0])[i]->id(),
+                                  (**books[0])[i+1]->id()}));
                 }
                 return new const_book(brs);
             } else {
---
+++ src/understand/recognize/recognizor.hpp
@@ -0,0 +1,19 @@
+#ifndef __recognize_hpp__
+#define __recognize_hpp__
+
+#include <lib/object/object.hpp>
+#include <lib/object/ptr.hpp>
+
+using namespace kxy;
+
+namespace mind {
+
+    class recognizor : public object {
+    public:
+        DECLARE_TYPE(object, "recognizor");
+
+    };
+}
+
+#endif
+
