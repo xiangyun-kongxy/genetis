b230da7ec3848b347f3a4a2d67601572561b383e
孔祥云 <kongxiangyun@kongxiayunsiMac.lan>
2018-06-12 23:21:37 +0800

    init

---
+++ src/ide/osx/genetis/genetis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  genetis
+//
+//  Created by 孔祥云 on 6/12/18.
+//  Copyright © 2018 孔祥云. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
